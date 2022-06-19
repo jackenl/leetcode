1285e961f4f6db5cb8f25046d299ca3c84fdda99
Zhijie Li <zhijie.li@shopee.com>
2022-06-19 18:12:59 +0800

    feat: update solutions

--- src/567.permutation-in-string.js
+++ src/567.permutation-in-string.js
@@ -11,20 +11,26 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-  const m = s1.length, n = s2.length;
+  const n1 = s1.length, n2 = s2.length;
   const map = new Map();
-  for (let i = 0; i < m; i++) {
-    map.set(s1[i], (map.get(s1[i]) || 0) + 1);
+  for (let i = 0; i < n1; i++) {
+    const cnt = map.get(s1[i]) || 0;
+    map.set(s1[i], cnt + 1);
   }
   let left = 0, right = 0;
-  while (right < n) {
-    map.set(s2[right], (map.get(s2[right]) || 0) - 1);
-    while (map.get(s2[right]) < 0) {
-      map.set(s2[left], map.get(s2[left]) + 1);
+  while (right < n2) {
+    const cnt = (map.get(s2[right]) || 0);
+    if (cnt === 0) {
+      while (s2[left] !== s2[right]) {
+        map.set(s2[left], map.get(s2[left]) + 1);
+        left++;
+      }
       left++;
+    } else {
+      map.set(s2[right], cnt - 1);
     }
+    if (right - left + 1 === n1) return true;
     right++;
-    if (right - left === m) return true;
   }
   return false;
 };
